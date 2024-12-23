79a30069d19b720e6477acc25463a92b213405eb
aditya <176797545+sk-adi@users.noreply.github.com>
2024-12-23 13:42:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
+import Card from './component/Card';
+import Carousel from './component/Carousel';
+import Pagination from './component/Pagination';
+import Trending from './component/Trending';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Carousel />
+      <Trending/>
+      <Pagination/>
+
+    </>
   );
 }
 
